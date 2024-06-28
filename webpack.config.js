a5ca77883bf13a462a32f56545fb241f5d9bf09e
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-28 18:32:00 +0530

    addded font

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
+    mode: "production",
 
-    entry: './src/index.js',
+    entry: "./src/index.js",
 
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Daily TODO"
+            title: "get ripped",
         }),
     ],
 
@@ -16,21 +16,21 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
             },
         ],
     },
 
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
 };
